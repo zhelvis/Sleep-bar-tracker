a19a4a2c47714259abd7e1ef2d950a9f433f1b1a
Vladimir Zhelvis <war1lok@yandex.ru>
2019-09-16 13:37:44 +0300

    server side API fetch

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const axios = require("axios").default;
+
 const app = express();
 
 const PORT = 3000;
+
+const API =
+  "https://api.weather.yandex.ru/v1/forecast?lat=55.75396&lon=37.620393&extra=false";
+const KEY = "8080807e-b853-454f-9ebf-32404a583769";
 
 const cors = require("cors");
 
@@ -10,6 +16,17 @@ app.use(express.static(__dirname + "/www"));
 
 app.get("/", (req, res) => {
   res.send("/www/index");
+});
+
+app.get("/forecast", async (req, res) => {
+  try {
+    const apiResponse = await axios.get(API, {
+      headers: { "X-Yandex-API-Key": KEY }
+    });
+    res.send(apiResponse.data);
+  } catch (e) {
+    res.status(500).send(`${e}`);
+  }
 });
 
 app.listen(PORT, () => {
